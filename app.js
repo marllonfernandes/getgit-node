180af93f9bad2e9643618f78330aa06943cd3690
marllonfernandes <marllonfernandes>
2019-07-19 22:17:51 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const workingDirPath = 'c:\\temp\\'
+const simpleGit = require('simple-git')(workingDirPath)
+const repositorio = 'https://github.com/marllonfernandes/getgit-node.git'
+
+app.get('/', (req, res, next) => {
+    res.end('hello world')
+    simpleGit.clone(repositorio)
+})
+
+app.listen(8091, () => { console.log('started application on port 8091') })
