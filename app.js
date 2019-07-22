cf2ccecfc9fce3b5295b4c41c2e3c2b177acba0b
Marllon Fernandes Da Silva <marllon.fernandes@totvs.com.br>
2019-07-22 14:43:05 -0300

    novos ajustes

--- app.js
+++ app.js
@@ -3,35 +3,70 @@ const app = express()
 const workingDirPath = 'c:\\temp\\'
 const git = require('simple-git/promise')
 const gitSimple = require('simple-git')
-// const repositorio = 'https://github.com/marllonfernandes/DevOpsTI-API.git'
 const dirPathApp = 'DevOpsTI-API'
 const process = require('child-process-promise')
-const exec = require('child-process-promise').exec;
+const exec = require('child-process-promise').exec
+const moment = require('moment')
+const fs = require('fs')
+const config = require('./config')
+// const ecosystemPM2 = fs.readFileSync('c:\\ecosystem.config.js',{encoding: 'utf-8'})
 
 app.get('/', (req, res, next) => {
     res.end('Welcome to the Build CI NodeJs Application')
 })
 app.get('/clone', async (req, res, next) => {
-    if (req.query.repository == null || req.query.repository == undefined || req.query.repository == '') {
-        res.status(400).json({ message: 'Inválid repository!' })
-    } else {
-        try {
-            var clone = await git(workingDirPath).clone(req.query.repository)
-            if (clone == '') {
-                command = await exec(`cd ${workingDirPath}${dirPathApp} && npm install`)
-                command = await exec(`cd ${workingDirPath}${dirPathApp} && npm start`)
-                if (command.stderr == '') {
-                    res.json({ messagem: command.stdout })
-                } else {
-                    res.json({ messagem: command.stderr })
-                }
+
+    var repository = req.query.repository
+    var path = req.query.path
+    var application = req.query.application
+    var dirDataAtual = moment(new Date(), "YYYY_MM_DD_HH_mm", "pt", true).format("YYYY_MM_DD_HH_mm")
+
+    if (application == null || application == undefined || application == '') {
+        res.json({ message: 'Inválid application!' })
+        return
+    }
+
+    if (repository == null || repository == undefined || repository == '') {
+        if (application == 'back') {
+            repository = config.repoBack
+        } else {
+            if (application == 'fronts') {
+                repository = config.repoFrontS
             } else {
-                res.json({ messagem: clone })
+                repository = config.repoFrontA
             }
-        } catch (error) {
-            res.status(500).json({ messagem: error.message })
         }
     }
+
+    if (path == null || path == undefined || path == '') {
+        if (application == 'back') {
+            path = config.pathApplicationBack
+        } else {
+            path = config.pathApplicationFront
+        }
+    }
+
+    if (!fs.existsSync(`${path}${dirDataAtual}`)) {
+        fs.mkdirSync(`${path}${dirDataAtual}`);
+    }
+
+    try {
+        var clone = await git(`${path}${dirDataAtual}`).clone(repository)
+        if (clone == '') {
+            var command = await exec(`cd ${path}${dirDataAtual}\\${repository.split('/')[repository.split('/').length-1]} && npm install`)
+            res.json({ messagem: command.stderr ? command.stderr : command.stdout })
+        } else {
+            res.json({ messagem: clone })
+        }
+    } catch (error) {
+        res.status(500).json({ messagem: error.message })
+    }
+
+})
+
+app.get('/install', async (req, res, next) => {
+    var command = await exec(`cd ${workingDirPath}${dirPathApp} && npm install`)
+    res.end('install')
 })
 
 app.get('/pull', async (req, res, next) => {
@@ -44,9 +79,9 @@ app.get('/started', async (req, res, next) => {
     try {
         exec(`cd ${workingDirPath}${dirPathApp} && npm start`)
         // exec(`${__dirname}\\started_app.bat`)
-        res.json({message: 'ok'})
+        res.json({ message: 'ok' })
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 
 })
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    pathApplicationBack: 'C:\\application\\BackApps\\',
+    pathApplicationFront: 'C:\\application\\WebApps\\',
+    repoBack: 'http://tfs2015.totvs.com.br:8080/tfs/TDITOTVS12/TI%20TOTVS/_git/PortalDevOpsAPI-Node',
+    repoFrontS: 'http://tfs2015.totvs.com.br:8080/tfs/TDITOTVS12/TI%20TOTVS/_git/PortalDevOpsServer',
+    
+    repoFrontA: 'http://tfs2015.totvs.com.br:8080/tfs/TDITOTVS12/TI%20TOTVS/_git/PortalDevopsFront',
+}
