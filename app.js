c68e936d7a769145e7365b417eaffaf1f0931df7
marllonfernandes <marllonfernandes>
2019-07-23 08:03:24 -0300

    ajustes

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
-const workingDirPath = 'c:\\temp\\'
 const git = require('simple-git/promise')
 const gitSimple = require('simple-git')
-const dirPathApp = 'DevOpsTI-API'
 const process = require('child-process-promise')
 const exec = require('child-process-promise').exec
 const moment = require('moment')
@@ -16,55 +14,55 @@ const { zip } = require('zip-a-folder')
 app.get('/', (req, res, next) => {
     res.end('Welcome to the Build CI NodeJs Application')
 })
-app.get('/clone', async (req, res, next) => {
+// app.get('/clone', async (req, res, next) => {
 
-    var repository = req.query.repository
-    var path = req.query.path
-    var application = req.query.application
-    var dirDataAtual = moment(new Date(), "YYYY_MM_DD_HH_mm", "pt", true).format("YYYY_MM_DD_HH_mm")
+//     var repository = req.query.repository
+//     var path = req.query.path
+//     var application = req.query.application
+//     var dirDataAtual = moment(new Date(), "YYYY_MM_DD_HH_mm", "pt", true).format("YYYY_MM_DD_HH_mm")
 
-    if (application == null || application == undefined || application == '') {
-        res.json({ message: 'Inválid application!' })
-        return
-    }
+//     if (application == null || application == undefined || application == '') {
+//         res.json({ message: 'Inválid application!' })
+//         return
+//     }
 
-    if (repository == null || repository == undefined || repository == '') {
-        if (application == 'back') {
-            repository = config.repoBack
-        } else {
-            if (application == 'fronts') {
-                repository = config.repoFrontS
-            } else {
-                repository = config.repoFrontA
-            }
-        }
-    }
+//     if (repository == null || repository == undefined || repository == '') {
+//         if (application == 'back') {
+//             repository = config.repoBack
+//         } else {
+//             if (application == 'fronts') {
+//                 repository = config.repoFrontS
+//             } else {
+//                 repository = config.repoFrontA
+//             }
+//         }
+//     }
 
-    if (path == null || path == undefined || path == '') {
-        if (application == 'back') {
-            path = config.pathApplicationBack
-        } else {
-            path = config.pathApplicationFront
-        }
-    }
+//     if (path == null || path == undefined || path == '') {
+//         if (application == 'back') {
+//             path = config.pathApplicationBack
+//         } else {
+//             path = config.pathApplicationFront
+//         }
+//     }
 
-    if (!fs.existsSync(`${path}${dirDataAtual}`)) {
-        fs.mkdirSync(`${path}${dirDataAtual}`);
-    }
+//     if (!fs.existsSync(`${path}${dirDataAtual}`)) {
+//         fs.mkdirSync(`${path}${dirDataAtual}`);
+//     }
 
-    try {
-        var clone = await git(`${path}${dirDataAtual}`).clone(repository)
-        if (clone == '') {
-            var command = await exec(`cd ${path}${dirDataAtual}\\${repository.split('/')[repository.split('/').length - 1]} && npm install`)
-            res.json({ messagem: command.stderr ? command.stderr : command.stdout })
-        } else {
-            res.json({ messagem: clone })
-        }
-    } catch (error) {
-        res.status(500).json({ messagem: error.message })
-    }
+//     try {
+//         var clone = await git(`${path}${dirDataAtual}`).clone(repository)
+//         if (clone == '') {
+//             var command = await exec(`cd ${path}${dirDataAtual}\\${repository.split('/')[repository.split('/').length - 1]} && npm install`)
+//             res.json({ messagem: command.stderr ? command.stderr : command.stdout })
+//         } else {
+//             res.json({ messagem: clone })
+//         }
+//     } catch (error) {
+//         res.status(500).json({ messagem: error.message })
+//     }
 
-})
+// })
 
 app.get('/pull', async (req, res, next) => {
 
@@ -97,15 +95,22 @@ app.get('/pull', async (req, res, next) => {
     var fileDestino = `${path}${repository.split('/')[repository.split('/').length - 1]}-bkp${dirDataAtual}.zip`
 
     // faz backup da aplicacao
-    await zip(diretorioOrigem, fileDestino);
-    
+    if (fs.existsSync(diretorioOrigem)) {
+        await zip(diretorioOrigem, fileDestino);
+    } else {
+        await git(path).clone(repository)
+    }
     // baixa do git os arquivos atualizados
     var resultGilPull = await gitPull(diretorioOrigem)
-    
-    // instala os pacotes e reinicia aplicacao
-    var command = await exec(`cd ${diretorioOrigem} && npm install && pm2 restart ${idapp}`)
 
-    res.json({message: 'pull repository'})
+    if (!resultGilPull == null || !resultGilPull == undefined) {
+        // instala os pacotes e reinicia aplicacao
+        var command = await exec(`cd ${diretorioOrigem} && npm install && pm2 restart ${idapp}`)
+        res.json({ message: 'Successfully updated application!' })
+    } else {
+        res.json({ message: 'There was no application update!' })
+    }
+
 })
 
 gitPull = (pathApp) => {
